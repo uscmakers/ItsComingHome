2c0e940659afa4e2ebf90bb4f54449a30a4b811a
Alex Colello <acolello@usc.edu>
2021-11-30 21:56:31 -0800

    Added code for firing

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template, redirect, url_for
+import RPi.GPIO as GPIO          
+from time import sleep
+
+in1 = 24
+in2 = 23
 
 app = Flask(__name__)
 
@@ -9,6 +14,11 @@ def index():
 @app.route('/fire')
 def fire():
     print('Fire!!!')
+    GPIO.output(in1,GPIO.HIGH)
+    GPIO.output(in2,GPIO.LOW)
+    sleep(3)
+    GPIO.output(in1,GPIO.LOW)
+    GPIO.output(in2,GPIO.HIGH)
     return redirect(url_for('success'))
 
 @app.route('/success')
@@ -16,4 +26,9 @@ def success():
     return render_template('success.html')
 
 if __name__ == '__main__':
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(in1,GPIO.OUT)
+    GPIO.setup(in2,GPIO.OUT)
+    GPIO.output(in1,GPIO.LOW)
+    GPIO.output(in2,GPIO.LOW)
     app.run(debug=True, host='0.0.0.0')
