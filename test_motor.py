0b2f76b04a586693a48fb91f7ed3a2a6183284db
Alex Colello <acolello@usc.edu>
2022-04-27 12:42:37 -0700

    Found correct timing values

--- test_motor.py
+++ test_motor.py
@@ -2,12 +2,21 @@ from webapp.spark import SparkController
 import RPi.GPIO as GPIO          
 from time import sleep
 
-pwm_pin = 10
+pwm_pin = 13
 
 GPIO.setmode(GPIO.BCM)
 controller = SparkController(pwm_pin)
-sleep(1)
-controller.set_speed(0.1)
 sleep(2)
-controller.nuetral()
-sleep(1)
+print('Forward')
+controller.set_speed(1)
+sleep(2)
+print('Neutral')
+controller.neutral()
+sleep(2)
+print('Backwards')
+controller.set_speed(-1)
+sleep(2)
+print('Neutral')
+controller.set_speed(0)
+sleep(2)
+
--- webapp/spark.py
+++ webapp/spark.py
@@ -1,15 +1,19 @@
 import RPi.GPIO as GPIO
 
+neutral_value = 7.1
+speed_coefficient = 2.5
+
 class SparkController:
     def __init__(self, pin):
         self.pin = pin
         GPIO.setup(self.pin, GPIO.OUT)
         self.pwm = GPIO.PWM(self.pin, 50)
-        self.pwm.start(7.5)
+        self.neutral()
 
-    def nuetral(self):
-        self.pwm.start(7.5)
+    def neutral(self):
+        self.pwm.start(neutral_value)
     
     # Speed should be a value -1 to 1
     def set_speed(self, speed):
-        self.pwm.start(7.5 + speed * 2.5)
+        speed = min(max(speed, -1.0), 1.0)
+        self.pwm.start(neutral_value + speed * speed_coefficient)
